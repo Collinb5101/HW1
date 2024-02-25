d369782a9be0cdc172fbea7e1cc47a2013b5588a
Collinb5101 <collinb5101@gmail.com>
2024-02-24 20:45:32 -0500

    coded up to the color tiles section

--- Assets/Scripts/Dijkstra.cs
+++ Assets/Scripts/Dijkstra.cs
@@ -2,6 +2,9 @@ using System.Diagnostics;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor.MemoryProfiler;
+using Unity.PlasticSCM.Editor.WebApi;
+using Unity.VisualScripting;
 
 /// <summary>
 /// Performs search using Dijkstra's algorithm.
@@ -21,8 +24,57 @@ public class Dijkstra : MonoBehaviour
     {
         // Starts the stopwatch.
         watch.Start();
-        
+
         // Add your Dijkstra code here.
+
+        //creates a new node record for start node
+        NodeRecord startRecord = new NodeRecord();
+        startRecord.node = start.GetComponent<Node>();
+        startRecord.connection = null;
+        startRecord.CostSoFar = 0;
+
+        //creates an open list of node records and adds the start node to it
+        List<NodeRecord> open = new List<NodeRecord>();
+        open.Add(startRecord);
+
+        //creates a closed list of node records
+        List<NodeRecord> closed = new List<NodeRecord>();
+
+        while(open.Count > 0)
+        {
+            //initialize a variable for the current node and lowest cost
+            NodeRecord currentNode= new NodeRecord();
+            float lowestCost = float.MaxValue;
+
+            //for every element inside the open list
+            for(int i = 0; i < open.Count; i++)
+            {
+                //if the current cost of this node is the lowest cost
+                if (open[i].CostSoFar < lowestCost)
+                {
+                    //this cost becomes the lowest cost and this node becomes the current node
+                    lowestCost = open[i].CostSoFar;
+                    currentNode = open[i];
+                }
+            }
+
+            //if colorTiles is true
+            if(colorTiles)
+            {
+                //color the current node to be the active node color
+                currentNode.ColorTile(activeColor);
+            }
+
+            //wait for specified amount of time
+            yield return new WaitForSeconds(waitTime);
+
+            //if the current node is the goal node
+            if(currentNode.node = end.GetComponent<Node>())
+            {
+                //break the while early
+                break;
+            }
+        }
 
         // Stops the stopwatch.
         watch.Stop();
@@ -48,6 +100,9 @@ public class NodeRecord
     public GameObject Tile { get; set; } = null;
 
     // Set the other class properties here.
+    public Node node = null;
+    public GameObject connection = null;
+    public float CostSoFar { get; set; } = 0;
 
     // Sets the tile's color.
     public void ColorTile (Color newColor)
